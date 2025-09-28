c6df464b2a24c216245de82cf3386b98bff5c988
Mokaddes-Ali <mokaddes.ru2000@gmail.com>
2025-09-28 21:47:17 +0600

    working rules page commit

---
+++ wp-content/themes/Smoke-N-Wings/sections/competition_awareness.php
@@ -0,0 +1,35 @@
+<section class="w-[1440px] flex gap-[34px] pt-16 pb-3 px-[125px]">
+
+  <!-- left image -->
+  <div class="w-[562.293px] relative">
+    <div class="absolute -top-1 -left-1 z-0">
+      <svg xmlns="http://www.w3.org/2000/svg" width="563" height="454" viewBox="0 0 563 454" fill="none">
+        <path d="M464.008 453.164H0V0.921112H562.293L464.008 453.164Z" fill="#591419"/>
+      </svg>
+    </div>
+
+    <!-- image -->
+    <div class="relative -top-3.5 -left-1 z-20">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/rule2.png"
+           alt="<?php esc_attr(the_title()) ?>"
+           class="w-[600.293px] h-[560.469px] object-cover z-20">
+    </div>
+  </div>
+
+  <!-- right side text -->
+  <div class="relative about-left py-8 pl-9 w-[600px] flex flex-col gap-9">
+
+    <!-- heading one line -->
+    <h2 class="text-[#16396F] font-bebas-pro text-[60px] font-bold leading-[81px] tracking-[1.56px] uppercase whitespace-nowrap">
+      The <span class="text-[#F65600]">competition</span> awareness
+    </h2>
+
+    <!-- paragraph -->
+    <p class="w-[570px] text-black font-jost text-[18px] font-normal leading-normal tracking-[0.36px]">
+      The competition will be held on the tarmac at the Blackfoot Municipal Airport (McCarley Field). During the competition the area will continue to be an active airport. No guns, fireworks and all fire/flame must be contained within the cooking apparatus. Pets MUST be on a leash or contained at the teams cook site. There is a dog park just outside the airport gate. Vehicles must follow established traffic flow through the airport property.
+      <br><br>
+      Each team site must have a fire extinguisher and if a stick burner is used some sort of ground protection must be used under the fire box. This may include sheet metal or cement board for.
+    </p>
+
+  </div>
+</section>
---
+++ wp-content/themes/Smoke-N-Wings/sections/competition_held_on.php
@@ -0,0 +1,39 @@
+<section class="relative w-[1440px] px-[125px] mx-auto pt-[105px] pb-10">
+
+    <!-- Foreground content layer -->
+    <div class="relative z-20 flex gap-12 w-full">
+            <!-- left side text -->
+            <div class="relative about-left py-4 w-[600px] flex flex-col gap-9">
+                <h2 class="text-[#16396F] font-bebas-pro text-[60px] font-bold leading-[81px] tracking-[1.56px] uppercase">
+                    The <span class="text-[#F65600]">competition</span> held on
+                </h2>
+
+                <p class="w-[570px] text-black font-jost text-[18px] font-normal leading-normal tracking-[0.36px]">
+                    The competition will be held on the tarmac at the Blackfoot Municipal Airport (McCarley Field).During the competition the area will continue to be an active airport.No guns, fireworks and all fire/flame must be contained within the cooking apparatus.Pets MUST be on a leash or contained at the teams cook site. There is a dog park just outside the airport gate.Vehicles must follow established traffic flow through the airport property.
+                    <br>
+                    <br>
+                    Each team site must have a fire extinguisher and if a stick burner is used some sort of ground protection must be used under the fire box. This may include sheet metal or cement board for.
+                </p>
+            </div>
+
+        <!-- right side image with SVG overlay -->
+        <div class="relative  w-[593px]">
+            <div class="relative w-full h-[495px]">
+                <div class="-ml-5">
+                <!-- svg -->
+                <svg xmlns="http://www.w3.org/2000/svg" width="594" height="495" viewBox="0 0 594 495" fill="none">
+                    <path d="M95 495H593.5V0H0L95 495Z" fill="#591419"/>
+                </svg> 
+                </div>
+
+                <div class="absolute -top-3 -left-9 -mr-3">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/rule1.png"
+                         alt="about image"
+                         class="w-[618px] h-[515px]">
+                </div>
+            </div>
+        </div>
+        </div>
+    </div>
+
+</section>
